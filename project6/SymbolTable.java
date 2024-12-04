d71e5eedbc9dfd69f1eafcef5ee2f5947afdbaa8
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2024-12-04 12:50:44 +0200

    finished symbol table

--- project6/SymbolTable.java
+++ project6/SymbolTable.java
@@ -10,11 +10,30 @@ public class SymbolTable {
         table = new HashMap<>();
 
         //we add predefined symbols 
+        addEntry("SCREEN", 16384);
+        addEntry("KBD", 24576);
+        addEntry("SP", 0);
+        addEntry("LCL", 1);
+        addEntry("ARG", 2);
+        addEntry("THIS", 3);
+        addEntry("THAT", 4);
         
-        
+        //register R0 to R15 usiing for loop
+        for (int i = 0; i<=15; i++){
+            addEntry("R"+i, i); 
+        }
     }
 
     public void  addEntry(String symbol, int address){
         table.put(symbol, address);
     }
+
+    public boolean contains(String symbol) {
+        return table.containsKey(symbol);
+    }
+
+    public int getAddress(String symbol){
+        return table.get(symbol);
+    }
+
 }
