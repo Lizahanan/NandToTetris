0e6606b9ca7494e76bbc6a646219eb5e82f6d912
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2024-12-14 20:40:03 +0200

    added project 7

--- project7/CodeWriter.java
+++ project7/CodeWriter.java
@@ -2,165 +2,190 @@
 import java.util.HashMap;
 import java.io.*;
 
-public class CodeWriter{
-    private BufferedWriter writer; //to write the Hack Assembly code
-    private int labelCounter; // To generate unique labels for branching
+public class CodeWriter {
+    private FileWriter writer;
+    private int boolCount;
+    private String fileName;
 
-    //constructor 
+    private static final HashMap<String, String> SEGMENT_BASE = new HashMap<>() {{
+        put("argument", "ARG");
+        put("local", "LCL");
+        put("this", "THIS");
+        put("that", "THAT");
+    }};
+
     public CodeWriter(String fileName) throws IOException {
-        writer = new BufferedWriter(new FileWriter(fileName));
-        labelCounter = 0;
+        initializeFile(fileName);
+        boolCount = 0;
     }
-
+    private void initializeFile(String fileName) throws IOException {
+        String path = fileName.replace(".vm", ".asm");
+        File outputFile = new File(path);
+        if (outputFile.createNewFile()) {
+            System.out.println("File created: " + outputFile.getName());
+        }
+        writer = new FileWriter(path);
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
     // writeArithmetic: Translates an arithmetic and logic command into Hack assembly
     public void writeArithmetic(String command) throws IOException{
-        switch (command){
+        if (requiresBinaryOperation(command)) {
+            popStackToD();
+        }
+        decrementSP();
+        setAToStack();
+        handleArithmeticCommand(command);
+        incrementSP();
+    }
+    private boolean requiresBinaryOperation(String command) {
+        return !command.equals("neg") && !command.equals("not");
+    }
+    private void handleArithmeticCommand(String command) throws IOException {
+        switch (command) {
             case "add":
-                writeBinaryOp("+");
+                writeArithmeticOperation("M=D+M");
                 break;
             case "sub":
-                writeBinaryOp("-");
-                break;
-            case "neg":     
-                writeUnaryOp("-");
-                break;           
-            case "eq":
-                writeComparison("JEQ");
-                break;           
-            case "gt":
-                writeComparison("JGT");
-                break; 
-            case "lt":
-                writeComparison("JLT");
+                writeArithmeticOperation("M=M-D");
                 break;
             case "and":
-                writeComparison("&");
+                writeArithmeticOperation("M=D&M");
                 break;
             case "or":
-                writeComparison("|");
+                writeArithmeticOperation("M=D|M");
+                break;
+            case "neg":
+                writeArithmeticOperation("M=-M");
                 break;
             case "not":
-                writeComparison("!");
+                writeArithmeticOperation("M=!M");
+                break;
+            case "eq":
+            case "gt":
+            case "lt":
+                handleBooleanCommand(command);
                 break;
             default:
                 throw new IllegalArgumentException("Invalid arithmetic command: " + command);
         }
     }
+    private void writeArithmeticOperation(String operation) throws IOException {
+        writer.write(operation + "\n");
+    }
+
+    private void handleBooleanCommand(String command) throws IOException {
+        writer.write("D=M-D\n");
+        writer.write("@BOOL" + boolCount + "\n");
+
+        String jumpCommand = switch (command) {
+            case "eq" -> "JEQ";
+            case "gt" -> "JGT";
+            case "lt" -> "JLT";
+            default -> throw new IllegalArgumentException("Invalid boolean command: " + command);
+        };
+        writer.write("D;" + jumpCommand + "\n");
+        setAToStack();
+        writer.write("M=0\n");
+        writer.write("@ENDBOOL" + boolCount + "\n");
+        writer.write("0;JMP\n");
+        writer.write("(BOOL" + boolCount + ")\n");
+        setAToStack();
+        writer.write("M=-1\n");
+        writer.write("(ENDBOOL" + boolCount + ")\n");
+        boolCount++;
+    }
+
 
     // writePushPop: Translates push or pop commands into Hack assembly
     public void writePushPop(String commandType , String segment , int index) throws IOException{
-        switch(commandType){
-            case "C_PUSH":
-                if (segment.equals("constant")) {
-                    writer.write("@" + index + "\n"); // Load the constant
-                    writer.write("D=A\n");          // D = constant
-                } else {
-                    loadSegmentAddress(segment, index);
-                    writer.write("D=M\n");          // D = *segment[index]
-                }
-                pushDToStack();
-                break;
-        
-            case "C_POP":
-                loadSegmentAddress(segment, index);
-                writer.write("D=A\n");              // D = address
-                writer.write("@R13\n");             // Use R13 as a temporary register
-                writer.write("M=D\n");              // R13 = address
-                popStackToD();
-                writer.write("@R13\n");
-                writer.write("A=M\n");              // A = address
-                writer.write("M=D\n");              // *address = D
-                break;
-
-            default:
-                throw new IllegalArgumentException("Invalid command type: " + commandType);
+        translateSegment(segment, index);
+        if (commandType.equals("C_PUSH")) {
+            handlePushCommand(segment);
+        } else if (commandType.equals("C_POP")) {
+            handlePopCommand();
+        } else {
+            throw new IllegalArgumentException("Invalid command type: " + commandType);
         }
     }
+    private void handlePushCommand(String segment) throws IOException {
+        if (segment.equals("constant")) {
+            writer.write("D=A\n");
+        } else {
+            writer.write("D=M\n");
+        }
+        pushDToStack();
+    }
+    private void handlePopCommand() throws IOException {
+        writer.write("D=A\n");
+        writer.write("@R13\n");
+        writer.write("M=D\n");
+        popStackToD();
+        writer.write("@R13\n");
+        writer.write("A=M\n");
+        writer.write("M=D\n");
+    }
+
 
     public void close() throws IOException{
         writer.close();
     }
-
-    //helper methods 
-    // writeBinaryOp: Writes the assembly code for binary operations (+, -, &, |)
-    private void writeBinaryOp(String operation) throws IOException {
-        popStackToD();
-        writer.write("A=A-1\n"); // Decrement the stack pointer
-        writer.write("M=M" + operation + "D\n"); // M = M operation D
-    }
-    // Writes a unary operation (-, !)
-    private void writeUnaryOp(String operation) throws IOException {
-        writer.write("@SP\n");
-        writer.write("A=M-1\n"); //point to the top of the stack
-        writer.write("M=" + operation + "M\n"); // M = operation M
-    }
-
-    // Writes a comparison (eq, gt, lt)
-    private void writeComparison(String jumpCommand) throws IOException {
-        popStackToD();
-        writer.write("A=A-1\n"); // Decrement the stack pointer
-        writer.write("D=M-D\n"); // D = M - D
-        writer.write("@TRUE" + labelCounter + "\n");
-        writer.write("D;" + jumpCommand + "\n"); // Jump if condition is true
-        writer.write("@SP\n");
-        writer.write("A=M-1\n"); // Point to the top of the stack
-        writer.write("M=0\n"); // M = false
-        writer.write("@CONTINUE" + labelCounter + "\n");
-        writer.write("0;JMP\n");
-        writer.write("(TRUE" + labelCounter + ")\n");
-        writer.write("@SP\n");
-        writer.write("A=M-1\n"); // Point to the top of the stack
-        writer.write("M=-1\n"); // M = true
-        writer.write("(CONTINUE" + labelCounter + ")\n");
-        labelCounter++;
+    private void translateSegment(String segment, int index) throws IOException {
+        switch (segment) {
+            case "constant":
+                writer.write("@" + index + "\n");
+                break;
+            case "static":
+                writer.write("@" + fileName + "." + index + "\n");
+                break;
+            case "temp":
+                writer.write("@R" + (5 + index) + "\n");
+                break;
+            case "pointer":
+                writer.write("@" + (index == 0 ? "THIS" : "THAT") + "\n");
+                break;
+            default:
+                translateBaseSegment(segment, index);
+        }
     }
 
-    // Pushes the value in D onto the stack
-    private void pushDToStack() throws IOException {
-        writer.write("@SP\n");
-        writer.write("A=M\n"); // Point to the stack pointer
-        writer.write("M=D\n"); // *SP = D
-        writer.write("@SP\n");
-        writer.write("M=M+1\n"); // Increment the stack pointer
-        
-    }
-
-    // Pops the top value of the stack into D
-    private void popStackToD() throws IOException {
-        writer.write("@SP\n");
-        writer.write("AM=M-1\n"); // Decrement the stack pointer and point to the top
-        writer.write("D=M\n"); // D = *SP
-       
-        
-    }
-
-    // Loads the address of a segment[index] into A
-    private void loadSegmentAddress(String segment, int index) throws IOException {
-        HashMap<String, String> segmentBase = new HashMap<>();
-        segmentBase.put("local", "LCL");
-        segmentBase.put("argument", "ARG");
-        segmentBase.put("this", "THIS");
-        segmentBase.put("that", "THAT");
-        if(segmentBase.containsKey(segment)){
-            writer.write("@" + segmentBase.get(segment) + "\n"); // Load the base address
-            writer.write("D=M\n"); // D = base address
-            writer.write("@" + index + "\n"); // Load the index
-            writer.write("A=D+A\n"); // A = base address + index
-        } else if (segment.equals("temp")){
-            writer.write("@R5\n"); //temp starts at R5
-            writer.write("D=A\n"); // D = 5
-            writer.write("@" + index + "\n"); // Load the index
-            writer.write("A=D+A\n"); // A = 5 + index
-        } else if (segment.equals("pointer")){
-            writer.write("@" + (index == 0 ? "THIS" : "THAT") + "\n"); // THIS or THAT
-        } else if (segment.equals("static")){
-            writer.write("@R16\n"); // Static variables start at R16
-            writer.write("D=A\n"); // D = 16
-            writer.write("@" + index + "\n"); // Load the index
-            writer.write("A=D+A\n"); // A = 16 + index
+    private void translateBaseSegment(String segment, int index) throws IOException {
+        if (SEGMENT_BASE.containsKey(segment)) {
+            writer.write("@" + SEGMENT_BASE.get(segment) + "\n");
+            writer.write("D=M\n");
+            writer.write("@" + index + "\n");
+            writer.write("A=D+A\n");
         } else {
             throw new IllegalArgumentException("Invalid segment: " + segment);
-        }    
+        }
     }
 
+    private void pushDToStack() throws IOException {
+        writer.write("@SP\n");
+        writer.write("A=M\n");
+        writer.write("M=D\n");
+        writer.write("@SP\n");
+        writer.write("M=M+1\n");
+    }
+
+    private void popStackToD() throws IOException {
+        writer.write("@SP\n");
+        writer.write("AM=M-1\n");
+        writer.write("D=M\n");
+    }
+
+    private void decrementSP() throws IOException {
+        writer.write("@SP\n");
+        writer.write("M=M-1\n");
+    }
+
+    private void incrementSP() throws IOException {
+        writer.write("@SP\n");
+        writer.write("M=M+1\n");
+    }
+    private void setAToStack() throws IOException {
+        writer.write("@SP\n");
+        writer.write("A=M\n");
+    }
 }
---
+++ project7/VMTranslator.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class VMTranslator {
+
+    public static void main(String[] args) throws IOException{
+        if (args.length != 1) {
+            System.err.println("Usage: java VMTranslator <file.vm>");
+            System.exit(1);
+        }
+
+        String inputFile = args[0];
+    
+        Parser parser = new Parser(inputFile);
+        CodeWriter writer = new CodeWriter(inputFile);
+
+        //process the file 
+        while(parser.hasMoreLines()){
+            parser.advance();
+            if (parser.commandType().equals("C_ARITHMETIC")){
+                writer.writeArithmetic(parser.arg1());
+            } else if (parser.commandType().equals("C_PUSH") || parser.commandType().equals("C_POP")){
+                writer.writePushPop(parser.commandType(), parser.arg1(), parser.arg2());
+            }
+        }
+        parser.close();
+        writer.close();
+        
+        System.out.println("Translation complete.");
+    }
+    
+}
